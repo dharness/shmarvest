b5a175def16357c67e8b77feb967e15d6f92ee85
Dylan Harness <dharness@uwo.ca>
2016-12-19 13:26:38 -0500

    renamed a few routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   get 'landing/index'
 
   root 'landing#index'
 
-  get '*unmatched_route', :to => 'application#raise_not_found!'
+  get '*unmatched_route', :to => 'landing#index'
 end
