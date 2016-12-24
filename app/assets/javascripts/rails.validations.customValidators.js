c7a85846dbaa7c7429df11e69a44a80d841d2d53
Dylan Harness <dharness@uwo.ca>
2016-12-24 18:19:28 -0500

    added uniqueness to web_address and email"

--- app/assets/javascripts/rails.validations.customValidators.js
+++ app/assets/javascripts/rails.validations.customValidators.js
@@ -1,7 +1,10 @@
-// window.ClientSideValidations.validators.local['web_address'] = function(element, options) {
-//   if (!/^[a-zA-Z0-9]*$/i.test(element.val())) {
-//     // When the value fails to pass validation you need to return the error message.
-//     // It can be derived from validator.message
-//     return options.message;
-//   }
-// }
+window.ClientSideValidations.validators.local['web_address'] = function(element, options) {
+  
+  console.log("Validating web address");
+
+  if (!/^[a-zA-Z0-9]*$/i.test(element.val())) {
+    // When the value fails to pass validation you need to return the error message.
+    // It can be derived from validator.message
+    return options.message;
+  }
+}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+class WebAddressValidator < ActiveModel::EachValidator
+  def validate_each(record, attr_name, value)
+    unless value =~ /^[a-zA-Z0-9]*$/i
+      record.errors.add(attr_name, :email, options.merge(value: value))
+    end
+  end
+end
+
 class User < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -7,13 +15,7 @@ class User < ApplicationRecord
     validates :first_name, :last_name, :company_name, :web_address, :presence => true
     validates :password, confirmation: false, presence: true,
                         length: { minimum: 8 }, on: :create
-    # validates_web_address :web_address
+    validates :web_address, uniqueness: true, :WebAddress => true
 
-#   def web_address_numbers_and_letters_only
-#       is_valid = /^[a-zA-Z0-9]*$/.match(web_address)
-#       if not is_valid
-#           errors.add(:invalid_web_address, "Letters and/or numbers only, please.")
-#       end
-#   end
-
+    validates_uniqueness_of :email
 end
