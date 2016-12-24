613789bb06d5b5ef4f0ed917b92307b5f7c25377
Dylan Harness <dharness@uwo.ca>
2016-12-24 00:27:30 -0500

    added more user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company_name, :contact_phone, :web_address, :team_size, :password, :email])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+    end
 end
