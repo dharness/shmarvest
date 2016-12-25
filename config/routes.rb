2bde297a8c3b2fef90d3b42c421530ae39330b57
Dylan Harness <dharness@uwo.ca>
2016-12-25 11:46:59 -0500

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
   
   get 'landing/index'
-  get '/welcome', to: 'welcome#index'
+
+  constraints subdomain: /./ do
+    get '/welcome', to: 'welcome#index'
+  end
 
   root 'landing#index'
 end
