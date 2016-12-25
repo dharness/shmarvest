1cc00e3cc58ccd47471cdf1e4c03e009b746caf0
Dylan Harness <dharness@uwo.ca>
2016-12-25 01:50:35 -0500

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
+  
   get 'landing/index'
+  get '/welcome', to: 'welcome#index'
 
   root 'landing#index'
-
-  # get '*unmatched_route', :to => 'landing#index'
 end
